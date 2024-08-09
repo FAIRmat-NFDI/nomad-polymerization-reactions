68e94992fe07a7da48fe9f1eb112ab3de3ce636f
Pepe Marquez <pepemarquezprieto@gmail.com>
2024-08-09 13:13:47 +0200

    Fix app test

--- tests/apps/test_app.py
+++ tests/apps/test_app.py
@@ -1,5 +1,3 @@
 def test_importing_app():
     # this will raise an exception if pydantic model validation fails for th app
-    from nomad_polymerization_reactions.apps.polymerization_reaction_app import (
-        polymerization_reaction_app,
-    )
+    from nomad_polymerization_reactions.apps import polymerization_reaction_app  # noqa: F401, I001

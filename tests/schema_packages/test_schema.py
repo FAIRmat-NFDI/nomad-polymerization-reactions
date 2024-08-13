1d5f8507ffb67018032068ce606b99eefe199e5d
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2024-08-13 15:26:41 +0200

    Update test schema

--- tests/schema_packages/test_schema.py
+++ tests/schema_packages/test_schema.py
@@ -4,10 +4,18 @@ from nomad.client import normalize_all, parse
 
 
 def test_schema():
-    test_file = os.path.join('tests', 'data', 'test.archive.yaml')
+    test_file = os.path.join(
+        'tests', 'data', 'test_polymerization_reaction.archive.yaml'
+    )
     entry_archive = parse(test_file)[0]
     normalize_all(entry_archive)
 
     assert (
-        entry_archive.data.DOI_number == 'https://doi.org/10.1002/macp.1985.021860810'
+        entry_archive.data.publication_reference.DOI_number
+        == 'https://doi.org/10.1002/macp.1985.021860810'
     )
+    assert (
+        entry_archive.data.publication_reference.publication_title
+        == 'Synthesis and characterization of a rubber incorporated polyamideimide'
+    )
+    assert entry_archive.data.monomers[0].pure_substance.name == 'Methacrylic acid'

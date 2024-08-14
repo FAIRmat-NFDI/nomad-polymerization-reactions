7f994d46233896f8a1d2e116b9dc533cad9e7ed8
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2024-08-14 18:34:59 +0200

    Add `AddPolymerizationReactions` section

--- src/nomad_polymerization_reactions/schema_packages/polymerization.py
+++ src/nomad_polymerization_reactions/schema_packages/polymerization.py
@@ -16,6 +16,7 @@ from nomad.datamodel.data import ArchiveSection, Schema
 from nomad.datamodel.metainfo.annotations import ELNAnnotation, ELNComponentEnum
 from nomad.datamodel.metainfo.basesections import (
     Activity,
+    BaseSection,
     CompositeSystem,
     PubChemPureSubstanceSection,
     PublicationReference,
@@ -123,6 +124,48 @@ class PolymerizationReactionReference(SectionReference):
         section_def=PolymerizationReaction,
         description='Reference to the polymerization reaction.',
     )
+
+
+class AddPolymerizationReactions(BaseSection, Schema):
+    """
+    A section to add polymerization reaction entries in the current upload.
+    Each entry is also referenced in this section.
+    """
+
+    zip_file = Quantity(
+        type=str,
+        description="""
+        Zip file containing data files in JSON format. Each file contains data for
+        one polymerization reaction.
+        """,
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.FileEditQuantity,
+        ),
+    )
+    polymerization_reactions = SubSection(
+        section_def=PolymerizationReactionReference,
+        repeats=True,
+        description='References to polymerization reaction entries.',
+    )
+    reprocess = Quantity(
+        type=bool,
+        description='Reprocess the zip file to generate entries.',
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.BoolEditQuantity,
+        ),
+        default=True,
+    )
+
+    def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
+        logger.info(
+            'AddPolymerizationReactions.normalize', parameter=configuration.parameter
+        )
+        super().normalize(archive, logger)
+        if self.zip_file and self.reprocess:
+            ## create entry archives with overwriting
+            ## use generate archive from json
+            ## add the entry to the polymerization_reactions reference list
+            self.reprocess = False
 
 
 m_package.__init_metainfo__()

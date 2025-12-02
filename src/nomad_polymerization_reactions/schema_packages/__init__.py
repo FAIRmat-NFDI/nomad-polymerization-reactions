4e125b10abf05a21119d35c132dfbb2f1bcfa03d
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2025-12-02 15:28:34 +0100

    Remove unwanted param in entry point

--- src/nomad_polymerization_reactions/schema_packages/__init__.py
+++ src/nomad_polymerization_reactions/schema_packages/__init__.py
@@ -1,10 +1,7 @@
 from nomad.config.models.plugins import SchemaPackageEntryPoint
-from pydantic import Field
 
 
 class MySchemaPackageEntryPoint(SchemaPackageEntryPoint):
-    parameter: int = Field(0, description='Custom configuration parameter')
-
     def load(self):
         from nomad_polymerization_reactions.schema_packages.polymerization import (
             m_package,

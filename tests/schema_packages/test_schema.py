ce45ab9da9836fe7f5eadb1b0a4b25f804ad75b3
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2025-12-04 15:10:21 +0100

    Fix pytest

--- tests/schema_packages/test_schema.py
+++ tests/schema_packages/test_schema.py
@@ -18,4 +18,4 @@ def test_schema():
         entry_archive.data.publication_reference.publication_title
         == 'Synthesis and characterization of a rubber incorporated polyamideimide'
     )
-    assert entry_archive.data.monomers[0].pure_substance.name == 'Methacrylic acid'
+    assert entry_archive.data.monomers[0].name == 'Methacrylic acid'

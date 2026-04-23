e8bb73a59f4554101d2cc251bc730871911a30d2
Sarthak Kapoor <57119427+ka-sarthak@users.noreply.github.com>
2026-04-23 16:47:19 +0200

    Refactor search app (#105)

--- src/nomad_polymerization_reactions/apps/__init__.py
+++ src/nomad_polymerization_reactions/apps/__init__.py
@@ -1,12 +1,9 @@
 from nomad.config.models.plugins import AppEntryPoint
 
-from nomad_polymerization_reactions.apps import polymerization
+from nomad_polymerization_reactions.apps.polymerization import polymerization_app
 
 polymerization = AppEntryPoint(
     name='Polymerization Reactions',
-    description="""This app allows you to search **polymerization reactions** within
-    NOMAD. The filter menu on the left and the shown default columns are specifically
-    designed for polymerization reaction exploration. The dashboard directly shows
-    useful interactive statistics about the data.""",
-    app=polymerization.app,
+    description='Search app for polymerization reactions entries.',
+    app=polymerization_app,
 )
--- src/nomad_polymerization_reactions/apps/polymerization.py
+++ src/nomad_polymerization_reactions/apps/polymerization.py
@@ -1,217 +1,336 @@
 import yaml
+from nomad.config.models.ui import (
+    App,
+    Axis,
+    Column,
+    Filters,
+    Format,
+    Menu,
+    MenuItemHistogram,
+    MenuItemTerms,
+    MenuSizeEnum,
+)
 
-app = yaml.safe_load(
-    """
-label: Polymerization Reaction
-path: polymerization
-category: Use Cases
-description: Search polymerization reactions
-readme: 'This page allows you to search **polymerization reactions**
-  within NOMAD. The dashboard directly shows useful
-  interactive statistics about the data.'
-filters:
-  include:
-  - '*#nomad_polymerization_reactions.schema_packages.polymerization.PolymerizationReaction'
-  exclude:
-  - mainfile
-  - entry_name
-  - combine
-filters_locked:
-  sections: nomad_polymerization_reactions.schema_packages.polymerization.PolymerizationReaction
-# pagination:
-  # order_by: data.reaction_conditions.solvent.smile#nomad_polymerization_reactions.schema_packages.polymerization.PolymerizationReaction
-search_syntaxes:
-  exclude:
-  - free_text
-columns:
-  selected:
-  - data.reaction_conditions.solvent.smile#nomad_polymerization_reactions.schema_packages.polymerization.PolymerizationReaction
-  - data.reaction_conditions.polymerization_method#nomad_polymerization_reactions.schema_packages.polymerization.PolymerizationReaction
-  - data.reaction_conditions.polymerization_type#nomad_polymerization_reactions.schema_packages.polymerization.PolymerizationReaction
-  - data.reaction_conditions.calculation_method#nomad_polymerization_reactions.schema_packages.polymerization.PolymerizationReaction
-  - data.reaction_conditions.temperature#nomad_polymerization_reactions.schema_packages.polymerization.PolymerizationReaction
-  - references
-  options:
-    data.reaction_conditions.solvent.smile#nomad_polymerization_reactions.schema_packages.polymerization.PolymerizationReaction: {label: Solvent}
-    data.reaction_conditions.polymerization_method#nomad_polymerization_reactions.schema_packages.polymerization.PolymerizationReaction: {label: Polymerization method}
-    data.reaction_conditions.polymerization_type#nomad_polymerization_reactions.schema_packages.polymerization.PolymerizationReaction: {label: Type}
-    data.reaction_conditions.calculation_method#nomad_polymerization_reactions.schema_packages.polymerization.PolymerizationReaction: {label: Calculation Method}
-    data.reaction_conditions.temperature#nomad_polymerization_reactions.schema_packages.polymerization.PolymerizationReaction:
-      format:
-        decimals: 2
-        mode: standard
-    #   label: Efficiency (%)
-    results.properties.optoelectronic.solar_cell.open_circuit_voltage:
-      format:
-        decimals: 3
-        mode: standard
-      unit: V
-    results.properties.optoelectronic.solar_cell.short_circuit_current_density:
-      format:
-        decimals: 3
-        mode: standard
-      unit: A/m**2
-    results.properties.optoelectronic.solar_cell.fill_factor:
-      format:
-        decimals: 3
-        mode: standard
-    references: {}
-    results.material.chemical_formula_hill:
-      label: Formula
-    results.material.structural_type: {}
-    results.properties.optoelectronic.solar_cell.illumination_intensity:
-      format:
-        decimals: 3
-        mode: standard
-      label: Illum. intensity
-      unit: W/m**2
-    results.eln.lab_ids: {}
-    results.eln.sections: {}
-    results.eln.methods: {}
-    results.eln.tags: {}
-    results.eln.instruments: {}
-    entry_name: {label: Name}
-    entry_type: {}
-    mainfile: {}
-    upload_create_time: {label: Upload time}
-    authors: {}
-    comment: {}
-    datasets: {}
-    published: {label: Access}
-filter_menus:
-  options:
-    material:
-      label: Absorber Material
-    elements:
-      label: Elements / Formula
-      level: 1
-      size: xl
-    structure:
-      label: Structure / Symmetry
-      level: 1
-    eln:
-      label: Electronic Lab Notebook
-    custom_quantities:
-      label: User Defined Quantities
-      size: l
-    author:
-      label: Author / Origin / Dataset
-      size: m
-    metadata:
-      label: Visibility / IDs / Schema
-    optimade:
-      label: Optimade
-      size: m
-# dashboard:
-#   widgets:
-#   - layout:
-#       lg: {h: 8, minH: 8, minW: 12, w: 12, x: 0, y: 0}
-#       md: {h: 8, minH: 8, minW: 12, w: 12, x: 0, y: 0}
-#       sm: {h: 8, minH: 8, minW: 12, w: 12, x: 0, y: 16}
-#       xl: {h: 8, minH: 8, minW: 12, w: 12, x: 0, y: 0}
-#       xxl: {h: 8, minH: 8, minW: 12, w: 13, x: 0, y: 0}
-#     quantity: results.material.elements
-#     scale: linear
-#     type: periodictable
-#   - autorange: true
-#     layout:
-#       lg: {h: 6, minH: 3, minW: 3, w: 12, x: 0, y: 8}
-#       md: {h: 6, minH: 3, minW: 3, w: 9, x: 0, y: 8}
-#       sm: {h: 5, minH: 3, minW: 3, w: 6, x: 0, y: 0}
-#       xl: {h: 8, minH: 3, minW: 3, w: 9, x: 12, y: 0}
-#       xxl: {h: 8, minH: 3, minW: 3, w: 12, x: 24, y: 0}
-#     markers:
-#       color:
-#         quantity: results.properties.optoelectronic.solar_cell.short_circuit_current_density
-#         unit: 'mA/cm^2'
-#     size: 1000
-#     type: scatterplot
-#     x:
-#       quantity: results.properties.optoelectronic.solar_cell.open_circuit_voltage
-#     y:
-#       quantity: results.properties.optoelectronic.solar_cell.efficiency
-#       title: 'Efficiency (%)'
-#   - autorange: true
-#     layout:
-#       lg: {h: 6, minH: 3, minW: 3, w: 12, x: 0, y: 14}
-#       md: {h: 6, minH: 3, minW: 3, w: 9, x: 9, y: 8}
-#       sm: {h: 5, minH: 3, minW: 3, w: 6, x: 6, y: 0}
-#       xl: {h: 8, minH: 3, minW: 3, w: 9, x: 21, y: 0}
-#       xxl: {h: 8, minH: 3, minW: 3, w: 11, x: 13, y: 0}
-#     markers:
-#       color:
-#         quantity: results.properties.optoelectronic.solar_cell.device_architecture
-#     size: 1000
-#     type: scatterplot
-#     x:
-#       quantity: results.properties.optoelectronic.solar_cell.open_circuit_voltage
-#     y:
-#       quantity: results.properties.optoelectronic.solar_cell.efficiency
-#       title: 'Efficiency (%)'
-#   - layout:
-#       lg: {h: 6, minH: 3, minW: 3, w: 6, x: 12, y: 0}
-#       md: {h: 4, minH: 3, minW: 3, w: 6, x: 12, y: 4}
-#       sm: {h: 6, minH: 3, minW: 3, w: 4, x: 0, y: 10}
-#       xl: {h: 6, minH: 3, minW: 3, w: 6, x: 14, y: 8}
-#       xxl: {h: 6, minH: 3, minW: 3, w: 6, x: 14, y: 8}
-#     quantity: results.properties.optoelectronic.solar_cell.device_stack
-#     scale: linear
-#     showinput: true
-#     type: terms
-#   - autorange: true
-#     layout:
-#       lg: {h: 4, minH: 3, minW: 3, w: 12, x: 12, y: 12}
-#       md: {h: 3, minH: 3, minW: 3, w: 8, x: 10, y: 17}
-#       sm: {h: 3, minH: 3, minW: 3, w: 8, x: 4, y: 13}
-#       xl: {h: 3, minH: 3, minW: 3, w: 8, x: 0, y: 11}
-#       xxl: {h: 3, minH: 3, minW: 3, w: 8, x: 0, y: 8}
-#     nbins: 30
-#     quantity: results.properties.optoelectronic.solar_cell.illumination_intensity
-#     scale: 1/4
-#     showinput: true
-#     type: histogram
-#   - layout:
-#       lg: {h: 6, minH: 3, minW: 3, w: 6, x: 18, y: 0}
-#       md: {h: 4, minH: 3, minW: 3, w: 6, x: 12, y: 0}
-#       sm: {h: 5, minH: 3, minW: 3, w: 4, x: 0, y: 5}
-#       xl: {h: 6, minH: 3, minW: 3, w: 6, x: 8, y: 8}
-#       xxl: {h: 6, minH: 3, minW: 3, w: 6, x: 8, y: 8}
-#     quantity: results.properties.optoelectronic.solar_cell.absorber_fabrication
-#     scale: linear
-#     showinput: true
-#     type: terms
-#   - autorange: false
-#     layout:
-#       lg: {h: 4, minH: 3, minW: 8, w: 12, x: 12, y: 16}
-#       md: {h: 3, minH: 3, minW: 8, w: 8, x: 10, y: 14}
-#       sm: {h: 3, minH: 3, minW: 8, w: 8, x: 4, y: 10}
-#       xl: {h: 3, minH: 3, minW: 8, w: 8, x: 0, y: 8}
-#       xxl: {h: 3, minH: 3, minW: 8, w: 8, x: 0, y: 11}
-#     nbins: 30
-#     quantity: results.properties.electronic.band_structure_electronic.band_gap.value
-#     title: 'Band gap'
-#     scale: 1/4
-#     showinput: false
-#     type: histogram
-#   - layout:
-#       lg: {h: 6, minH: 3, minW: 3, w: 6, x: 18, y: 6}
-#       md: {h: 6, minH: 3, minW: 3, w: 5, x: 0, y: 14}
-#       sm: {h: 5, minH: 3, minW: 3, w: 4, x: 4, y: 5}
-#       xl: {h: 6, minH: 3, minW: 3, w: 5, x: 25, y: 8}
-#       xxl: {h: 6, minH: 3, minW: 3, w: 6, x: 20, y: 8}
-#     quantity: results.properties.optoelectronic.solar_cell.electron_transport_layer
-#     scale: linear
-#     showinput: true
-#     type: terms
-#   - layout:
-#       lg: {h: 6, minH: 3, minW: 3, w: 6, x: 12, y: 6}
-#       md: {h: 6, minH: 3, minW: 3, w: 5, x: 5, y: 14}
-#       sm: {h: 5, minH: 3, minW: 3, w: 4, x: 8, y: 5}
-#       xl: {h: 6, minH: 3, minW: 3, w: 5, x: 20, y: 8}
-#       xxl: {h: 6, minH: 3, minW: 3, w: 6, x: 26, y: 8}
-#     quantity: results.properties.optoelectronic.solar_cell.hole_transport_layer
-#     scale: linear
-#     showinput: true
-#     type: terms
-# """  # noqa: E501
+PR_SCHEMA_PATH = (
+    'nomad_polymerization_reactions.schema_packages.polymerization.'
+    'PolymerizationReaction'
+)
+polymerization_app = App(
+    label='Polymerization Reactions',
+    path='polymerization',
+    category='Use Cases',
+    description='Search app for polymerization reactions entries.',
+    readme="""
+    Explore all the polymerization reactions with entry type
+    **`PolymerizationReaction`**. The filter menu on the left contains several pre-set
+    fields that are specifically designed for polymerization reaction exploration.
+    The dashboard directly shows useful interactive statistics about the data.
+    """,
+    filters=Filters(
+        include=[
+            f'*#{PR_SCHEMA_PATH}',
+        ],
+    ),
+    filters_locked={
+        'entry_type': 'PolymerizationReaction',
+    },
+    columns=[
+        Column(
+            search_quantity=(
+                f'data.reaction_conditions.polymerization_method#{PR_SCHEMA_PATH}'
+            ),
+            label='Polymerization Method',
+            selected=True,
+        ),
+        Column(
+            search_quantity=(
+                f'data.reaction_conditions.polymerization_type#{PR_SCHEMA_PATH}'
+            ),
+            label='Type',
+            selected=True,
+        ),
+        Column(
+            search_quantity=f'data.reaction_conditions.solvent.name#{PR_SCHEMA_PATH}',
+            label='Solvent',
+            selected=True,
+        ),
+        Column(
+            search_quantity=(f'data.reaction_conditions.temperature#{PR_SCHEMA_PATH}'),
+            label='Temperature',
+            selected=True,
+            options={
+                'format': Format(
+                    decimals=2,
+                    mode='standard',
+                )
+            },
+        ),
+        Column(
+            search_quantity=(f'data.publication_reference.DOI_number#{PR_SCHEMA_PATH}'),
+            label='Publication DOI',
+            selected=True,
+        ),
+    ],
+    menu=Menu(
+        size=MenuSizeEnum.SM,
+        items=[
+            Menu(
+                title='Reaction Conditions',
+                size=MenuSizeEnum.XXL,
+                items=[
+                    MenuItemHistogram(
+                        title='Temperature',
+                        x=Axis(
+                            search_quantity=(
+                                f'data.reaction_conditions.temperature#{PR_SCHEMA_PATH}'
+                            ),
+                            scale='linear',
+                            unit='K',
+                        ),
+                    ),
+                    MenuItemHistogram(
+                        title='Solvent LogP',
+                        x=Axis(
+                            search_quantity=(
+                                'data.reaction_conditions.solvent_descriptors.log_P#'
+                                f'{PR_SCHEMA_PATH}'
+                            ),
+                            scale='linear',
+                        ),
+                    ),
+                    MenuItemTerms(
+                        title='Solvent',
+                        search_quantity=(
+                            f'data.reaction_conditions.solvent.name#{PR_SCHEMA_PATH}'
+                        ),
+                        width=4,
+                        options=10,
+                        show_input=True,
+                    ),
+                    MenuItemTerms(
+                        title='Method',
+                        search_quantity=(
+                            'data.reaction_conditions.polymerization_method#'
+                            f'{PR_SCHEMA_PATH}'
+                        ),
+                        width=4,
+                        options=10,
+                        show_input=True,
+                    ),
+                    MenuItemTerms(
+                        title='Type',
+                        search_quantity=(
+                            'data.reaction_conditions.polymerization_type#'
+                            f'{PR_SCHEMA_PATH}'
+                        ),
+                        width=4,
+                        options=10,
+                        show_input=True,
+                    ),
+                ],
+            ),
+            Menu(
+                title='Reaction Constants',
+                size=MenuSizeEnum.XXL,
+                items=[
+                    MenuItemTerms(
+                        title='Calculation Method',
+                        search_quantity=(
+                            'data.reaction_conditions.calculation_method#'
+                            f'{PR_SCHEMA_PATH}'
+                        ),
+                        width=4,
+                        options=10,
+                        show_input=True,
+                    ),
+                    MenuItemHistogram(
+                        title='Reaction Constant',
+                        x=Axis(
+                            search_quantity=(
+                                'data.reaction_conditions.reaction_constants.'
+                                f'reaction_constant#{PR_SCHEMA_PATH}'
+                            ),
+                            scale='linear',
+                        ),
+                        width=8,
+                    ),
+                ],
+            ),
+            Menu(
+                title='Publication',
+                size=MenuSizeEnum.XXL,
+                items=[
+                    MenuItemHistogram(
+                        title='Publication Date',
+                        x=Axis(
+                            search_quantity=(
+                                'data.publication_reference.publication_date'
+                                f'#{PR_SCHEMA_PATH}'
+                            ),
+                            scale='linear',
+                        ),
+                    ),
+                    MenuItemTerms(
+                        title='DOI Number',
+                        search_quantity=(
+                            f'data.publication_reference.DOI_number#{PR_SCHEMA_PATH}'
+                        ),
+                        width=6,
+                        options=10,
+                        show_input=True,
+                    ),
+                    MenuItemTerms(
+                        title='Journal',
+                        search_quantity=(
+                            f'data.publication_reference.journal#{PR_SCHEMA_PATH}'
+                        ),
+                        width=6,
+                        options=10,
+                        show_input=True,
+                    ),
+                ],
+            ),
+        ],
+    ),
+    dashboard={
+        'widgets': yaml.safe_load(f"""
+- type: scatter_plot
+  autorange: true
+  sample_size: 10000
+  y:
+    search_quantity: data.r_product#{PR_SCHEMA_PATH}
+    title: r-product
+  x:
+    search_quantity: data.reaction_conditions.temperature#{PR_SCHEMA_PATH}
+    title: Reaction Temperature
+  title: r-product vs Reaction Temperature
+  layout:
+    xxl:
+      minH: 3
+      minW: 3
+      h: 7
+      w: 10
+      y: 0
+      x: 0
+    xl:
+      minH: 3
+      minW: 3
+      h: 9
+      w: 12
+      y: 0
+      x: 11
+    lg:
+      minH: 3
+      minW: 3
+      h: 6
+      w: 9
+      y: 0
+      x: 9
+    md:
+      minH: 3
+      minW: 3
+      h: 6
+      w: 9
+      y: 0
+      x: 0
+    sm:
+      minH: 3
+      minW: 3
+      h: 6
+      w: 9
+      y: 0
+      x: 0
+- type: terms
+  scale: linear
+  search_quantity: data.monomers.name#{PR_SCHEMA_PATH}
+  title: Monomer
+  layout:
+    xxl:
+      minH: 3
+      minW: 3
+      h: 7
+      w: 7
+      y: 0
+      x: 19
+    xl:
+      minH: 3
+      minW: 3
+      h: 9
+      w: 6
+      y: 0
+      x: 23
+    lg:
+      minH: 3
+      minW: 3
+      h: 9
+      w: 6
+      y: 0
+      x: 18
+    md:
+      minH: 3
+      minW: 3
+      h: 9
+      w: 6
+      y: 0
+      x: 0
+    sm:
+      minH: 3
+      minW: 3
+      h: 9
+      w: 6
+      y: 0
+      x: 0
+- type: scatter_plot
+  autorange: true
+  sample_size: 10000
+  markers:
+    color:
+      search_quantity: data.reaction_conditions.calculation_method#{PR_SCHEMA_PATH}
+      title: Calculation Method
+  y:
+    search_quantity: data.reaction_conditions.reaction_constants[0].reaction_constant#{PR_SCHEMA_PATH}
+    title: r1
+  x:
+    search_quantity: data.reaction_conditions.reaction_constants[1].reaction_constant#{PR_SCHEMA_PATH}
+    title: r2
+  title: "r1 vs r2 colored by Calculation Method"
+  layout:
+    xxl:
+      minH: 3
+      minW: 3
+      h: 7
+      w: 9
+      y: 0
+      x: 10
+    xl:
+      minH: 3
+      minW: 3
+      h: 9
+      w: 11
+      y: 0
+      x: 0
+    lg:
+      minH: 3
+      minW: 3
+      h: 6
+      w: 9
+      y: 0
+      x: 0
+    md:
+      minH: 3
+      minW: 3
+      h: 6
+      w: 9
+      y: 0
+      x: 0
+    sm:
+      minH: 3
+      minW: 3
+      h: 6
+      w: 9
+      y: 0
+      x: 0
+
+""")  # noqa: E501
+    },
 )

7719a746404c90002370d3a350457b281c6f9337
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2026-05-26 18:38:32 +0200

    Title harmonization

--- src/nomad_polymerization_reactions/apps/polymerization.py
+++ src/nomad_polymerization_reactions/apps/polymerization.py
@@ -114,7 +114,7 @@ polymerization_app = App(
                         show_input=True,
                     ),
                     MenuItemTerms(
-                        title='Method',
+                        title='Polymerization Method',
                         search_quantity=(
                             'data.reaction_conditions.polymerization_method#'
                             f'{PR_SCHEMA_PATH}'

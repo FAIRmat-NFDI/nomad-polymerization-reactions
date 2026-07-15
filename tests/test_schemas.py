426791f0c056c5534ecad118120c039e7463c6fc
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2026-07-15 10:02:08 +0200

    chore: add test for monomer

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -19,3 +19,14 @@ def test_polymerization_reaction():
         == 'Synthesis and characterization of a rubber incorporated polyamideimide'
     )
     assert entry_archive.data.monomers[0].name == 'Methacrylic acid'
+
+def test_monomer():
+    test_file = os.path.join(
+        'tests', 'data', 'test_monomer.archive.yaml'
+    )
+    entry_archive = parse(test_file)[0]
+    normalize_all(entry_archive)
+
+    assert entry_archive.data.pure_substance.smile == 'C=CC#N'
+    assert entry_archive.data.pure_substance.pub_chem_cid == 7855
+    assert entry_archive.data.name == 'Acrylonitrile'
